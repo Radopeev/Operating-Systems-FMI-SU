f09078e18b956f21480281b6c34006537f4b7883
Radoslav Peev <95441540+Radopeev@users.noreply.github.com>
2024-06-20 17:29:19 +0300

    Update 2017-SE-01.c

--- Exams/C/Input_output/2017-SE-01.c
+++ Exams/C/Input_output/2017-SE-01.c
@@ -1,56 +1,88 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <errno.h>
 #include <err.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/stat.h>
 
-void print_file(int fd,int number_lines,int* line_count);
-void print_file(int fd, int number_lines, int* line_count) {
-        char buff[1024];
-        int read_size;
-        while((read_size=read(fd,&buff,sizeof(buff) - 1 )) > 0) {
-                buff[read_size] = '\0';
 
-                if(number_lines) {
-                        dprintf(STDOUT_FILENO,"%d %s",(*line_count)++,buff);
-                } else {
-                        dprintf(STDOUT_FILENO,"%s", buff);
-                }
-        }
+typedef struct tuple {
+        uint16_t offset;
+        uint8_t original;
+        uint8_t new;
+} tuple;
 
-        if(read_size == -1){
-                err(1,"adssd");
-        }
+void close_all(int f1,int f2,int patch);
+void close_all(int f1,int f2,int patch){
+        close(f1);
+        close(f2);
+        close(patch);
 }
 
-
-int main(int argc, char* argv[]) {
-        int number_lines = 0;
-        int line_count = 0;
-        int file_start_index;
-
-        if(argc > 2 && strcmp(argv[1],"-n") == 0) {
-                number_lines = 1;
-                file_start_index=3;
+int main(int argc, char* argv[]){
+        if(argc != 4){
+                errx(1,"asdsad");
         }
 
-        if(file_start_index == argc) {
-                print_file(STDIN_FILENO,number_lines,&line_count);
-        } else {
-                for(int i = file_start_index;i < argc;i++) {
-                        if(strcmp(argv[i],"-") == 0) {
-                                print_file(STDIN_FILENO,number_lines, &line_count);
-                        } else {
-                                int fd = open(argv[i],O_RDONLY);
-                                if(fd < 0){
-                                        err(1,"adsasdsa");
-                                }
+        int f1 = open(argv[1], O_RDONLY);
 
-                                print_file(fd,number_lines,&line_count);
-                                close(fd);
+        if(f1 < 0) {
+                err(1,"asds");
+        }
+
+        int f2 = open(argv[2], O_RDONLY);
+
+        if(f2 < 0) {
+                close(f1);
+                err(1,"asdd");
+        }
+
+        int patch = open(argv[3],O_WRONLY | O_CREAT | O_TRUNC,0644);
+
+        if(patch < 0) {
+                close(f2);
+                err(1,"asdsd");
+        }
+
+        uint8_t byte_1,byte_2;
+
+        struct stat st_1,st_2;
+
+        if(fstat(f1,&st_1) < 0) {
+                close_all(f1,f2,patch);
+                err(1,"asdsad");
+        }
+
+        if(fstat(f2,&st_2) < 0) {
+                close_all(f1,f2,patch);
+                err(1,"asds");
+        }
+
+        if(st_1.st_size != st_2.st_size) {
+                errx(1,"asdsd");
+        }
+
+        int count = 0;
+
+        while(read(f1,&byte_1,sizeof(uint8_t) > 0)) {
+                if(read(f2,&byte_2,sizeof(uint8_t)) <=  0) {
+                        close_all(f1,f2,patch);
+                        err(1,"adsdasd");
+                }
+
+                if(byte_1 != byte_2) {
+                        tuple t;
+                        t.offset = count;
+                        t.original = byte_1;
+                        t.new = byte_2;
+                        if(write(patch,&t,sizeof(t)) < 0) {
+                                close_all(f1,f2,patch);
+                                err(1,"adsada");
                         }
                 }
+                count++;
         }
+
+        close_all(f1,f2,patch);
 }
+
